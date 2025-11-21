3d4911969619f1d18b196623219d79276bb24db0
miya256 <miyatomo02@au.com>
2025-11-22 02:55:25 +0900

    ファイル構成変更

---
+++ library/integer/README.md
@@ -0,0 +1,15 @@
+# 整数系
+
+## number_theory
+数論
+素数、約数、gcd、CRT、メビウス関数、NTT
+
+## bit
+bit操作
+
+## set
+集合のゼータメビウス変換
+Zobrist Hash
+
+## combinatorics
+二項係数mod、next permutation, ベル数
---
+++ library/range_query/sparse_table.py
@@ -0,0 +1,17 @@
+class SparseTable:
+    """
+    冪等性が必要なのでmax,minとかはOK、sumとかはダメ
+    """
+    def __init__(self, op: object, data: list) -> None:
+        self.op = op
+        self.data = [[(v, i) for i, v in enumerate(data)]]
+        for i in range(1, len(data).bit_length()):
+            self.data.append([0] * (len(data)-(1<<i)+1))
+            for j in range(len(data)-(1<<i)+1):
+                self.data[i][j] = self.op(self.data[i-1][j], self.data[i-1][j+(1<<(i-1))])
+    
+    def prod(self, l: int, r: int) -> tuple[int, int]:
+        """[l,r)までopした結果(値, index)"""
+        assert 0 <= l <= r <= len(self.data), f"index error [l,r)=[{l},{r})"
+        idx = (r-l).bit_length() - 1
+        return self.op(self.data[idx][l], self.data[idx][r-(1<<idx)])
