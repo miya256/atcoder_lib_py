a8a8f16a32bdfd1358e9efacccc2ba2f6984e020
miya256 <miyatomo02@au.com>
2025-11-27 00:30:50 +0900

    linear eq

--- library/number/linear_algebra/gaussian_elimination.py
+++ library/number/linear_algebra/gaussian_elimination.py
@@ -15,3 +15,33 @@ def gaussian_eliminate(a: Matrix) -> int:
         rank += 1
     return rank
 
+
+def solve_linear_eq(a: Matrix, b: Matrix) -> list[list[int]] | None:
+    c = a.join_columns(b)
+    rank = gaussian_eliminate(c)
+
+    # rank以降の行はすべて0になるはずなので、そうでなかったら解なし
+    if any(c[i, a.m] for i in range(rank, a.n)):
+        return None
+    
+    res = [[0] * a.m for _ in range(a.m - rank + 1)]
+    const = []
+    var = []
+
+    # 定数項、未定定数とそうでないものを決める
+    i = 0
+    for j in range(a.m):
+        if c[i, j] == 1:
+            res[0][j] = c[i, a.m]
+            var.append(j)
+            i += 1
+        else:
+            const.append(j)
+    
+    for i, j in enumerate(const, 1): # 未定定数xjが係数のベクトルを求める
+        res[i][j] = 1
+        for k, v in enumerate(var):
+            if v > j: # j以降はすべて0だから
+                break
+            res[i][v] = -c[k, j] % a.Mod
+    return res
