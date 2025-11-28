3da441e51c7fb99def69fc8d576573119b350471
miya256 <miyatomo02@au.com>
2025-11-28 21:12:07 +0900

    persistent stack

--- library/persistent/persistent_stack.py
+++ library/persistent/persistent_stack.py
@@ -1,36 +1,50 @@
 class PersistentStack:
+    """
+    永続スタック
+
+    Methods:
+        get     : 末尾を取得
+        push    : 要素を追加
+        pop     : 末尾を取り出す
+        save    : 状態を保存
+        load    : 状態をkeyに
+        is_empty: 空か
+    """
     class Node:
-        def __init__(self, value):
+        def __init__(self, value: object) -> None:
             self.value = value
             self.parent = None
 
-    def __init__(self):
-        self.tail = None
-        self.version = dict()
+    def __init__(self) -> None:
+        self._tail = None
+        self._version = dict()
     
-    def get(self):
+    def get(self) -> object:
         """末尾の要素を取得"""
-        return self.tail.value
+        return self._tail.value
     
-    def push(self,value):
-        new = self.Node(value)
-        new.parent = self.tail
-        self.tail = new
+    def push(self, value: object) -> None:
+        """追加"""
+        new = PersistentStack.Node(value)
+        new.parent = self._tail
+        self._tail = new
     
-    def pop(self):
-        assert self.tail,"stack is empty"
-        value = self.tail.value
-        self.tail = self.tail.parent
+    def pop(self) -> object:
+        """末尾の要素を取り出す"""
+        assert self._tail,"stack is empty"
+        value = self._tail.value
+        self._tail = self._tail.parent
         return value
     
-    def save(self,key):
+    def save(self, key: object) -> None:
         """keyに今の状態を保存"""
-        self.version[key] = self.tail
+        self._version[key] = self._tail
     
-    def load(self,key):
+    def load(self, key: object) -> None:
         """stackをkeyの状態にする"""
-        assert key in self.version,f'{key}に対応するversionは存在しません'
-        self.tail = self.version[key]
+        assert key in self._version,f"version {key} does not exist"
+        self._tail = self._version[key]
     
-    def isempty(self):
-        return self.tail is None
+    def is_empty(self) -> bool:
+        """stackが空か"""
+        return self._tail is None
