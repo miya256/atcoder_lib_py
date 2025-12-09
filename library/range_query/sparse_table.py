6af405479adf1d20f7952ae878734489f8e64f3d
miya256 <miyatomo02@au.com>
2025-12-10 01:48:27 +0900

    sparse table update

--- library/range_query/sparse_table.py
+++ library/range_query/sparse_table.py
@@ -5,7 +5,8 @@ class SparseTable:
     冪等性が必要なのでmax,minとかはOK、sumとかはダメ
     """
     def __init__(self, op: Callable, data: list) -> None:
-        self.op = op
+        # max,minであるindexがわかるために、indexも含めたopにしている
+        self.op = lambda x, y: x if op(x[0], y[0]) == x[0] else y
         self.data = [[(v, i) for i, v in enumerate(data)]]
         for i in range(1, len(data).bit_length()):
             self.data.append([0] * (len(data)-(1<<i)+1))
