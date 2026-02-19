458c9a9a7e2f72966adbac0ed2f7d941f0ab4298
miya256 <miyatomo02@au.com>
2026-02-20 00:51:43 +0900

    length sum 消した

--- library/range_query/interval_set.py
+++ library/range_query/interval_set.py
@@ -8,7 +8,6 @@ class IntervalSet:
 
     def __init__(self) -> None:
         self._data = SortedSet()
-        self._sum_length = 0
     
     def __iter__(self):
         it = iter(self._data)
@@ -28,6 +27,13 @@ class IntervalSet:
         i = self._data.bisect_right(x)
         return i % 2
     
+    def get_interval(self, x: int) -> tuple[int, int] | None:
+        """xを含む区間を返す"""
+        i = self._data.bisect_right(x)
+        if i % 2 == 0: # xは区間に含まれない
+            return None
+        return self._data[i-1], self._data[i]
+    
     def add(self, l: int, r: int) -> None:
         """区間[l, r)を追加"""
         assert l < r, f"Invalid interval: [l,r)=[{l},{r})"
@@ -36,14 +42,10 @@ class IntervalSet:
         j = self._data.bisect_right(r)
         j += j % 2
         for _ in range(i, j, 2):
-            tl = self._data.pop(i)
-            tr = self._data.pop(i)
-            self._sum_length -= tr - tl
-            l = min(l, tl)
-            r = max(r, tr)
+            l = min(l, self._data.pop(i))
+            r = max(r, self._data.pop(i))
         self._data.add(l)
         self._data.add(r)
-        self._sum_length += r - l
 
     def discard(self, l: int, r: int) -> None:
         """区間[l, r)を追加"""
@@ -58,12 +60,19 @@ class IntervalSet:
     
     def flip(self, l: int, r: int) -> None:
         """区間[l, r)を反転"""
+        assert l < r, f"Invalid interval: [l,r)=[{l},{r})"
         self._data.discard(l) if l in self._data else self._data.add(l)
         self._data.discard(r) if r in self._data else self._data.add(r)
 
-
     def mex(self, x: int) -> int:
         """x 以上で区間に含まれない最小の数"""
+        i = self._data.bisect_right(x)
+        return self._data[i] if i % 2 else x
+    
+    def sum(self, l: int, r: int) -> int:
+        """[l, r)の中の区間の長さの合計"""
+        # 高速に求められたらいいなあ
+        return 0
 
 
 s = IntervalSet()
