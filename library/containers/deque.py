338c54e1fb0d7b3e00845d837d22f3a79ac04d74
miya256 <miyatomo02@au.com>
2026-02-26 18:16:50 +0900

    fix: containers

--- library/containers/deque.py
+++ library/containers/deque.py
@@ -12,28 +12,29 @@ class Deque:
         popleft()      : 左から取り出す
         pop()          : 右から取り出す
     """
+
     def __init__(self, data: list = []) -> None:
-        self._buffer = data + [None] * (1<<20 - len(data))
+        self._buffer = data + [None] * (1 << 20 - len(data))
         self._head = 0
         self._tail = len(data)
         self._cur = 0
-    
+
     def __getitem__(self, i: int) -> object:
-        """"i番目を取得"""
+        """ "i番目を取得"""
         return self.get(i)
-    
+
     def __setitem__(self, i: int, val: object) -> None:
         """i番目をvalに"""
         self.set(i, val)
-    
+
     def __len__(self) -> int:
         """dequeのサイズ"""
         return self._tail - self._head
-    
+
     def __contains__(self, val: object) -> bool:
         """valがあるか"""
         return val in self._buffer
-    
+
     def __iter__(self):
         self._cur = 0
         return self
@@ -44,66 +45,69 @@ class Deque:
             self._cur += 1
             return val
         raise StopIteration
-    
+
     def __repr__(self):
-        return f'Deque({list(self)})'
-    
+        return f"Deque({list(self)})"
+
     def get(self, i: int) -> object:
-        """"i番目を取得"""
+        """ "i番目を取得"""
         assert -len(self) <= i < len(self), f"index {i} out of range"
         return self._buffer[self._index(i)]
-    
+
     def set(self, i: int, val: object) -> None:
         """i番目をvalに"""
         assert -len(self) <= i < len(self), f"index {i} out of range"
         self._buffer[self._index(i)] = val
-    
+
     def is_full(self) -> bool:
         """すべて埋まっているか"""
         return len(self) == len(self._buffer)
-    
+
     def is_empty(self) -> bool:
         """空か"""
         return len(self) == 0
-    
+
     def appendleft(self, val: object) -> None:
         """左に追加"""
         if self.is_full():
             self._extend()
         self._head -= 1
         self._buffer[self._head % len(self._buffer)] = val
-    
+
     def append(self, val: object) -> None:
         """右に追加"""
         if self.is_full():
             self._extend()
         self._buffer[self._tail % len(self._buffer)] = val
         self._tail += 1
-    
+
     def popleft(self) -> object:
         """左の要素を取り出す"""
         assert not self.is_empty(), "deque is empty"
         val = self._buffer[self._head % len(self._buffer)]
         self._head += 1
         return val
-    
+
     def pop(self) -> object:
         """右の要素を取り出す"""
         assert not self.is_empty(), "deque is empty"
         self._tail -= 1
         val = self._buffer[self._tail % len(self._buffer)]
         return val
-    
+
     def _index(self, i: int) -> int:
         if i >= 0:
             return (self._head + i) % len(self._buffer)
         else:
             return (self._tail + i) % len(self._buffer)
-    
+
     def _extend(self) -> None:
         head = self._head
         buflen = len(self._buffer)
-        self._buffer = self._buffer[head % buflen:] + self._buffer[:head % buflen] + [None]*buflen
+        self._buffer = (
+            self._buffer[head % buflen :]
+            + self._buffer[: head % buflen]
+            + [None] * buflen
+        )
         self._head -= head
         self._tail -= head
-        
--- library/containers/heap/heap.py
+++ library/containers/heap/heap.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+
 class Heap:
     """
     優先度付きキュー（ヒープ）
@@ -8,23 +9,25 @@ class Heap:
         add(value): 追加
         pop()     : 削除
     """
+
     def __init__(self, compare: Callable[[object, object], bool]) -> None:
-        self._heap = [None]
+        self._heap: list[object] = [None]
         self._compare = compare
-    
+
     def __len__(self) -> int:
         """長さ"""
         return len(self._heap) - 1
-    
-    def __getitem__(self, i: int) -> int:
+
+    def __getitem__(self, i: int) -> object:
         """i番目の要素"""
-        return self._heap[i+1]
-    
+        assert 0 <= i < len(self), f"index out of range: i={i}"
+        return self._heap[i + 1]
+
     def add(self, value: object) -> None:
         """valueを追加"""
         self._heap.append(value)
         self._sift_up(len(self))
-    
+
     def pop(self) -> object:
         """0番目を取り出す"""
         assert len(self) > 0, "heap is empty"
@@ -33,27 +36,32 @@ class Heap:
         self._heap.pop()
         self._sift_down(1)
         return res
-    
+
     def _sift_up(self, i: int) -> None:
         while i > 1:
-            if self._compare(self._heap[i//2], self._heap[i]):
+            if self._compare(self._heap[i // 2], self._heap[i]):
                 break
-            self._heap[i//2], self._heap[i] = self._heap[i], self._heap[i//2]
+            self._heap[i // 2], self._heap[i] = self._heap[i], self._heap[i // 2]
             i >>= 1
-    
+
     def _sift_down(self, i: int) -> None:
         while True:
             smallest = i
-            if i*2 <= len(self) and self._compare(self._heap[i*2], self._heap[smallest]):
-                smallest = i*2
-            if i*2+1 <= len(self) and self._compare(self._heap[i*2+1], self._heap[smallest]):
-                smallest = i*2+1
-            
+            if i * 2 <= len(self) and self._compare(
+                self._heap[i * 2], self._heap[smallest]
+            ):
+                smallest = i * 2
+            if i * 2 + 1 <= len(self) and self._compare(
+                self._heap[i * 2 + 1], self._heap[smallest]
+            ):
+                smallest = i * 2 + 1
+
             if smallest == i:
                 break
             self._heap[i], self._heap[smallest] = self._heap[smallest], self._heap[i]
             i = smallest
 
+
 def compare(parent: object, child: object) -> bool:
     """parent が child より優先されるなら True"""
-    return parent < child
+    return parent < child  # type: ignore
