d2f186cd541b339f2bc19cdac60c31c3cab52942
miya256 <miyatomo02@au.com>
2026-02-27 01:09:32 +0900

    fix: range_query

--- library/range_query/interval_set.py
+++ library/range_query/interval_set.py
@@ -1,5 +1,6 @@
 from sortedcontainers import SortedSet
 
+
 class IntervalSet:
     """
     区間を管理する[l, r)
@@ -16,7 +17,7 @@ class IntervalSet:
 
     def __init__(self) -> None:
         self._data = SortedSet()
-    
+
     def __iter__(self):
         it = iter(self._data)
         while True:
@@ -26,7 +27,7 @@ class IntervalSet:
                 yield (l, r)
             except StopIteration:
                 return
-            
+
     def __or__(self, other: "IntervalSet") -> "IntervalSet":
         new_set = IntervalSet()
         for l, r in self:
@@ -34,13 +35,13 @@ class IntervalSet:
         for l, r in other:
             new_set.add(l, r)
         return new_set
-    
+
     def __and__(self, other: "IntervalSet") -> "IntervalSet":
         new_set = IntervalSet()
         i = j = 0
         while i < len(self._data) and j < len(other._data):
-            l1, r1 = self._data[i], self._data[i+1]
-            l2, r2 = other._data[j], other._data[j+1]
+            l1, r1 = self._data[i], self._data[i + 1]
+            l2, r2 = other._data[j], other._data[j + 1]
             l = max(l1, l2)
             r = min(r1, r2)
             if l < r:
@@ -59,7 +60,7 @@ class IntervalSet:
         for l, r in other:
             new_set.flip(l, r)
         return new_set
-    
+
     def __sub__(self, other: "IntervalSet") -> "IntervalSet":
         new_set = IntervalSet()
         for l, r in self:
@@ -67,29 +68,29 @@ class IntervalSet:
         for l, r in other:
             new_set.discard(l, r)
         return new_set
-    
+
     def __ior__(self, other: "IntervalSet") -> "IntervalSet":
         for l, r in other:
             self.add(l, r)
         return self
-    
+
     def __iand__(self, other: "IntervalSet") -> "IntervalSet":
         self._data = (self & other)._data
         return self
-    
+
     def __ixor__(self, other: "IntervalSet") -> "IntervalSet":
         for l, r in other:
             self.flip(l, r)
         return self
-    
+
     def __isub__(self, other: "IntervalSet") -> "IntervalSet":
         for l, r in other:
             self.discard(l, r)
         return self
-    
+
     def __repr__(self) -> str:
         return f"{[(l, r) for l, r in self]}"
-    
+
     def add(self, l: int, r: int) -> None:
         """区間[l, r)を追加"""
         assert l < r, f"Invalid interval: [l,r)=[{l},{r})"
@@ -113,30 +114,30 @@ class IntervalSet:
         # 戻す
         self._data.pop(0)
         self._data.pop()
-    
+
     def flip(self, l: int, r: int) -> None:
         """区間[l, r)を反転"""
         assert l < r, f"Invalid interval: [l,r)=[{l},{r})"
         self._data.discard(l) if l in self._data else self._data.add(l)
         self._data.discard(r) if r in self._data else self._data.add(r)
-    
+
     def contains(self, x: int) -> bool:
         """区間にxが含まれるか"""
         i = self._data.bisect_right(x)
         return i % 2
-    
+
     def get_interval(self, x: int) -> tuple[int, int] | None:
         """xを含む区間を返す"""
         i = self._data.bisect_right(x)
-        if i % 2 == 0: # xは区間に含まれない
+        if i % 2 == 0:  # xは区間に含まれない
             return None
-        return self._data[i-1], self._data[i]
+        return self._data[i - 1], self._data[i]
 
     def mex(self, x: int) -> int:
         """x 以上で区間に含まれない最小の数"""
         i = self._data.bisect_right(x)
         return self._data[i] if i % 2 else x
-    
+
     def sum(self, l: int, r: int) -> int:
         """[l, r)の中の区間の長さの合計"""
         # 高速に求められたらいいなあ
--- library/range_query/sparse_table.py
+++ library/range_query/sparse_table.py
@@ -1,20 +1,27 @@
 from typing import Callable
 
+
 class SparseTable:
     """
     冪等性が必要なのでmax,minとかはOK、sumとかはダメ
     """
+
     def __init__(self, op: Callable, data: list) -> None:
         # max,minであるindexがわかるために、indexも含めたopにしている
         self.op = lambda x, y: x if op(x[0], y[0]) == x[0] else y
-        self.data = [[(v, i) for i, v in enumerate(data)]]
+        self.data: list[list[tuple[object, int]]] = [
+            [(v, i) for i, v in enumerate(data)]
+        ]
         for i in range(1, len(data).bit_length()):
-            self.data.append([0] * (len(data)-(1<<i)+1))
-            for j in range(len(data)-(1<<i)+1):
-                self.data[i][j] = self.op(self.data[i-1][j], self.data[i-1][j+(1<<(i-1))])
-    
+            data_i: list[tuple[object, int]] = [(0, 0)] * (len(data) - (1 << i) + 1)
+            for j in range(len(data) - (1 << i) + 1):
+                data_i[j] = self.op(
+                    self.data[i - 1][j], self.data[i - 1][j + (1 << (i - 1))]
+                )
+            self.data.append(data_i)
+
     def prod(self, l: int, r: int) -> tuple[int, int]:
         """[l,r)までopした結果(値, index)"""
         assert 0 <= l <= r <= len(self.data), f"index error [l,r)=[{l},{r})"
-        idx = (r-l).bit_length() - 1
-        return self.op(self.data[idx][l], self.data[idx][r-(1<<idx)])
+        idx = (r - l).bit_length() - 1
+        return self.op(self.data[idx][l], self.data[idx][r - (1 << idx)])
