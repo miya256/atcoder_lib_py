5843b91d30df459a7b28d2c43dc782dd9b902eda
miya256 <miyatomo02@au.com>
2026-02-26 18:46:03 +0900

    fix: dp

--- library/dp/bounded_knapsack.py
+++ library/dp/bounded_knapsack.py
@@ -1,20 +1,21 @@
 from collections import deque
 
-n,W = map(int,input().split())
-dp = [0] * (W+1)
+n, W = map(int, input().split())
+dp = [0] * (W + 1)
 for i in range(n):
-    v, w, c = map(int,input().split())
-    ndp = [0] * (W+1)
-    for r in range(w): # wで割った余りがrの遷移をやる
+    v, w, c = map(int, input().split())
+    ndp = [0] * (W + 1)
+    for r in range(w):  # wで割った余りがrの遷移をやる
         dq = deque()
-        adjusted = lambda j: dp[j] - j // w * v # max(dp[kw+r]-kv)を求めたいから
+        adjusted = lambda j: dp[j] - j // w * v  # max(dp[kw+r]-kv)を求めたいから
         # スライド最大値
-        for j in range(r, W+1, w): # ndp[j]を求める
-            while dq and dq[0] < j - c*w: # 最大c個までだからそれより前の値はもういらない
+        for j in range(r, W + 1, w):  # ndp[j]を求める
+            # 最大c個までだからそれより前の値はもういらない
+            while dq and dq[0] < j - c * w:
                 dq.popleft()
             while dq and adjusted(dq[-1]) < adjusted(j):
                 dq.pop()
             dq.append(j)
-            ndp[j] = adjusted(dq[0]) + j // w * v # max(dp[kw+r]-kv) + tv
+            ndp[j] = adjusted(dq[0]) + j // w * v  # max(dp[kw+r]-kv) + tv
     dp = ndp
 print(max(dp))
