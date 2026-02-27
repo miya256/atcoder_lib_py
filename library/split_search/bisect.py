28f97f718f68ca4b052913118716e5a3cf7c9648
miya256 <miyatomo02@au.com>
2026-02-28 03:21:51 +0900

    fix: split search

--- library/split_search/bisect.py
+++ library/split_search/bisect.py
@@ -1,19 +1,21 @@
 from typing import Callable
 
+
 def bisect_left(a: list, x, key: Callable | None = None) -> int:
     l, r = -1, len(a)
-    while r-l > 1:
-        mid = (l+r)//2
+    while r - l > 1:
+        mid = (l + r) // 2
         if (key is None and a[mid] < x) or (key is not None and key(a[mid]) < key(x)):
             l = mid
         else:
             r = mid
     return r
 
+
 def bisect_right(a: list, x, key=None) -> int:
     l, r = -1, len(a)
-    while r-l > 1:
-        mid = (l+r)//2
+    while r - l > 1:
+        mid = (l + r) // 2
         if (key is None and a[mid] <= x) or (key is not None and key(a[mid]) <= key(x)):
             l = mid
         else:
--- library/split_search/ternary_search.py
+++ library/split_search/ternary_search.py
@@ -1,13 +1,16 @@
-def ternary_search(l: int, r: int) -> int:
+from typing import Callable
+
+
+def ternary_search(l: float, r: float, f: Callable) -> float:
     """
     三分探索
     狭義の単調性が必要
     """
-    while r-l > 1e-9:
-        ml = (l*2 + r) // 3
-        mr = (l + r*2) // 3
+    while r - l > 1e-9:
+        ml = (l * 2 + r) / 3
+        mr = (l + r * 2) / 3
         if f(ml) > f(mr):
             l = ml
         else:
             r = mr
-    return
+    return l
