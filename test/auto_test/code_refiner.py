2f54cdbb5e53500f07f8d461ee648dd114979ce9
miya256 <miyatomo02@au.com>
2026-02-28 14:58:41 +0900

    関数だけ定義

--- test/auto_test/code_refiner.py
+++ test/auto_test/code_refiner.py
@@ -8,6 +8,16 @@ def remove_assert(src_lines: list[str]) -> list[str]:
     return [line for line in src_lines if not pattern.match(line)]
 
 
+def remove_type_hints(src_lines: list[str]) -> list[str]:
+    """型ヒントを取り除く"""
+    return src_lines
+
+
+def remove_unused_imports(src_lines: list[str]) -> list[str]:
+    """不要なimportを取り除く"""
+    return src_lines
+
+
 def refine_code(src_lines: list[str]) -> list[str]:
     src_lines = remove_assert(src_lines)
     return src_lines
