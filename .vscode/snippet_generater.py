9bd3ab7c1e4fcc3d0656327dd8b0950ab386fffc
miya256 <miyatomo02@au.com>
2026-03-02 02:09:00 +0900

    update snippets

--- .vscode/snippet_generater.py
+++ .vscode/snippet_generater.py
@@ -2,25 +2,27 @@ import re
 import json
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-files = [
-    "../library/compression/run_length_encoding.py",
-    "../library/compression/coordinate_compression.py",
-    "../library/connectivity/union_find.py",
-    "../library/graph/graph.py",
-    "../library/graph/search/bfs.py",
-    "../library/number/factor/divisor.py",
-    "../library/number/mod/binomial.py",
-    "../library/number/tools/ring.py",
-    "../library/range_query/fenwick_tree/fenwick_tree.py",
-    "../library/range_query/segment_tree/segment_tree.py",
-    "../library/range_query/lazy_segment_tree/lazy_segment_tree.py",
-    "../library/split_search/binary_search.py",
-    "../library/string/rolling_hash.py",
-    "../library/string/trie.py",
-    "../library/string/z_algorithm.py",
-]
+exclude = set([
+    "dp/bounded_knapsack.py",
+    "graph/directed/scc_tarjan.py",
+    "graph/search/bipartite.py",
+    "graph/search/dfs.py",
+    "number/factor/linear_sieve.py",
+    "number/fps/fps.py",
+    "number/linear_algebra/matrix_multiple_strassen.py",
+    "number/mod/mod_pow.py",
+    "number/tools/bit_reverse.py",
+    "number/tools/bit_tricks.py",
+    "number/tools/popcount.py",
+    "range_query/sparse_table.py",
+    "split_search/bisect.py",
+    "string/aho_corasick.py",
+    "string/suffix_array.py",
+])
+
+exclude = [Path(path) for path in exclude]
 
 def extract_definitions(lines: list[str]) -> tuple[str, str]:
     for line in lines:
@@ -31,22 +33,33 @@ def extract_definitions(lines: list[str]) -> tuple[str, str]:
             return "class", class_match.group(1)
         elif func_match:
             return "def", func_match.group(1)
+    raise Exception("関数・クラスが見つかりませんでした")
 
 
 def main():
     snippets = {}
-    for path in files:
-        full_path = (BASE_DIR / path).resolve()
-        with open(full_path, "r", encoding="utf-8") as f:
+    root = BASE_DIR / "library"
+    for path in root.rglob("*"):
+        if not path.is_file():
+            continue
+        if path.suffix != ".py":
+            continue
+        if path.relative_to(root) in exclude:
+            continue
+
+        with open(path, "r", encoding="utf-8") as f:
             lines: list[str] = f.readlines()
-        type, name = extract_definitions(lines)
-        snippets[name] = {
-            "prefix": f"{type} {name}",
-            "body": [line.rstrip("\n") for line in lines],
-            "description": f"Auto snippet for {name}"
-        }
+        try:
+            type, name = extract_definitions(lines)
+            snippets[name] = {
+                "prefix": f"{type} {name}",
+                "body": [line.rstrip("\n") for line in lines],
+                "description": f"Auto snippet for {name}"
+            }
+        except Exception as e:
+            print(f"{e}: {path.relative_to(root)}")
     
-    snippets_dir = BASE_DIR / "lib.code-snippets"
+    snippets_dir = BASE_DIR / ".vscode/lib.code-snippets"
     with open(snippets_dir, "w", encoding="utf-8") as f:
         json.dump(snippets, f, indent=2, ensure_ascii=False)
 
