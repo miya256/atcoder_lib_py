8f3ab209492d513f94e2c6b3cf856b5c56e8658e
miya256 <miyatomo02@au.com>
2026-03-12 20:38:26 +0900

    graph/shortest_path and tree

--- library/graph/tree/diameter.py
+++ library/graph/tree/diameter.py
@@ -1,3 +1,6 @@
+from ..graph import Graph
+
+
 class Diameter(Graph):
     """
     木の直径
@@ -6,9 +9,10 @@ class Diameter(Graph):
         diameter(): 直径と端点を返す
         dist(v)   : vからほかの点までの距離
     """
+
     def __init__(self, n: int) -> None:
-        super().__init__(n, n-1)
-    
+        super().__init__(n, n - 1)
+
     def _dfs(self, v: int, dist: list[int] | None = None) -> tuple[int, int]:
         """distを指定すればvからの距離、そうでなければ直径と端点"""
         stack = [(v, -1, 0)]
@@ -25,18 +29,18 @@ class Diameter(Graph):
 
             for v, w in self(u):
                 if v != par:
-                    stack.append((v, u, d+w))
-        
+                    stack.append((v, u, d + w))
+
         return diameter, end
-    
+
     def diameter(self) -> tuple[int, int, int]:
         """直径、端点1、端点2"""
         _, end1 = self._dfs(0)
         diameter, end2 = self._dfs(end1)
         return diameter, end1, end2
-    
+
     def dist(self, v: int) -> list[int]:
         """vからの距離"""
-        dist = [inf := 1<<61] * self.n
+        dist = [inf := 1 << 61] * self.n
         self._dfs(v, dist)
         return dist
