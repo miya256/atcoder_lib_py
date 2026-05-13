2332ea86200e1e05786b667795ba5bce83c502db
miya256 <miyatomo02@au.com>
2026-05-13 21:58:36 +0900

    chore: ufをElementを返すようにした

--- tools/library_checker/accumulate.test.py
+++ tools/library_checker/accumulate.test.py
@@ -1,11 +1,11 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/static_range_sum
 
-from library.range_query.accumulate import Accumulate
+from library.range_query.accumulate import Accumulation
 
 n, q = map(int, input().split())
 a = list(map(int, input().split()))
-acc = Accumulate(a)
+acc = Accumulation(a)
 
 for _ in range(q):
     l, r = map(int, input().split())
-    print(acc.sum([l], [r]))
+    print(acc.sum((l,), (r,)))
